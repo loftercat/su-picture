f013eb08d4406f5f6e44d876a97cbea51aebf19f
lofter <723202177@qq.com>
2026-06-27 13:12:11 +0800

    新增Cos下载能力，测试接口

--- src/main/java/com/su/supicturebackend/controller/FileController.java
+++ src/main/java/com/su/supicturebackend/controller/FileController.java
@@ -1,5 +1,7 @@
 package com.su.supicturebackend.controller;
 
+import com.qcloud.cos.model.COSObject;
+import com.qcloud.cos.utils.IOUtils;
 import com.su.supicturebackend.annotation.AuthCheck;
 import com.su.supicturebackend.common.BaseResponse;
 import com.su.supicturebackend.common.ResultUtils;
@@ -8,14 +10,18 @@ import com.su.supicturebackend.exception.BusinessException;
 import com.su.supicturebackend.exception.ErrorCode;
 import com.su.supicturebackend.manager.CosManager;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.OutputStream;
 
 @Slf4j
 @RestController
@@ -46,6 +52,21 @@ public class FileController {
                     log.error("delete file failed = " + filepath);
                 }
             }
+        }
+    }
+
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    @GetMapping("/test/download")
+    public void download(@RequestParam("filepath") String filepath, HttpServletResponse response) {
+        try (COSObject cosObject = cosManager.getObject(filepath)) {
+            OutputStream outputStream = response.getOutputStream();
+            response.setContentType("application/octet-stream");
+            response.setHeader("Content-Disposition", "attachment; filename=\"" + filepath + "\"");
+            IOUtils.copy(cosObject.getObjectContent(), outputStream);
+            outputStream.flush();
+        } catch (Exception e) {
+            log.error("download file failed = " + filepath, e);
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "下载文件失败");
         }
     }
 }
--- src/main/java/com/su/supicturebackend/manager/CosManager.java
+++ src/main/java/com/su/supicturebackend/manager/CosManager.java
@@ -1,6 +1,8 @@
 package com.su.supicturebackend.manager;
 
 import com.qcloud.cos.COSClient;
+import com.qcloud.cos.model.COSObject;
+import com.qcloud.cos.model.GetObjectRequest;
 import com.qcloud.cos.model.PutObjectRequest;
 import com.qcloud.cos.model.PutObjectResult;
 import com.su.supicturebackend.config.CosClientConfig;
@@ -22,6 +24,12 @@ public class CosManager {
     public PutObjectResult putObject(String key, File file) {
         PutObjectRequest putObjectRequest = new PutObjectRequest(cosClientConfig.getBucket(), key, file);
         return cosClient.putObject(putObjectRequest);
+    }
+
+    // 从 COS 下载对象
+    public COSObject getObject(String key) {
+        GetObjectRequest getObjectRequest = new GetObjectRequest(cosClientConfig.getBucket(), key);
+        return cosClient.getObject(getObjectRequest);
     }
 
 }
